c92bccf271dbae9c1a181eccec1dbe5441c33ded
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:16:49 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IcommerceepaycoDatabaseSeeder.php
+++ Database/Seeders/IcommerceepaycoDatabaseSeeder.php
@@ -5,70 +5,75 @@ namespace Modules\Icommerceepayco\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Icommerce\Entities\PaymentMethod;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IcommerceepaycoDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-      
-      Model::unguard();
-  
-      $this->call(IcommerceepaycoModuleTableSeeder::class);
-      $name = config('asgard.icommerceepayco.config.paymentName');
-      $result = PaymentMethod::where('name',$name)->first();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
 
-      if(!$result){
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Icommerceepayco\Database\Seeders",
+      "seeds" => ["IcommerceepaycoModuleTableSeeder"]
+    ]);
 
-        $options['init'] = "Modules\Icommerceepayco\Http\Controllers\Api\IcommerceEpaycoApiController";
-        $options['mainimage'] = null;
-        $options['publicKey'] = null;
-        $options['clientId'] = null;
-        $options['pKey'] = null;
-        $options['autoClick'] = true;
-        $options['test'] = true;
-        $options['minimunAmount'] = 15000;
-        $options['showInCurrencies'] = ["COP"];
-  
-        $titleTrans = 'Epayco';
-        $descriptionTrans = 'icommerceepayco::icommerceepaycos.description';
+    $name = config('asgard.icommerceepayco.config.paymentName');
+    $result = PaymentMethod::where('name', $name)->first();
 
-        $params = array(
-          'name' => $name,
-          'status' => 1,
-          'options' => $options
-        );
-        $paymentMethod = PaymentMethod::create($params);
+    if (!$result) {
 
-        $this->addTranslation($paymentMethod,'en',$titleTrans,$descriptionTrans);
-        $this->addTranslation($paymentMethod,'es',$titleTrans,$descriptionTrans);
+      $options['init'] = "Modules\Icommerceepayco\Http\Controllers\Api\IcommerceEpaycoApiController";
+      $options['mainimage'] = null;
+      $options['publicKey'] = null;
+      $options['clientId'] = null;
+      $options['pKey'] = null;
+      $options['autoClick'] = true;
+      $options['test'] = true;
+      $options['minimunAmount'] = 15000;
+      $options['showInCurrencies'] = ["COP"];
 
-      }else{
+      $titleTrans = 'Epayco';
+      $descriptionTrans = 'icommerceepayco::icommerceepaycos.description';
 
-        $this->command->alert("This method has already been installed !!");
+      $params = array(
+        'name' => $name,
+        'status' => 1,
+        'options' => $options
+      );
+      $paymentMethod = PaymentMethod::create($params);
 
-      }
-   
-    }
+      $this->addTranslation($paymentMethod, 'en', $titleTrans, $descriptionTrans);
+      $this->addTranslation($paymentMethod, 'es', $titleTrans, $descriptionTrans);
 
+    } else {
 
-    /*
-    * Add Translations
-    * PD: New Alternative method due to problems with astronomic translatable
-    **/
-    public function addTranslation($paymentMethod,$locale,$title,$description){
-
-      \DB::table('icommerce__payment_method_translations')->insert([
-          'title' => $title,
-          'description' => trans($description,[],$locale),
-          'payment_method_id' => $paymentMethod->id,
-          'locale' => $locale
-      ]);
+      $this->command->alert("This method has already been installed !!");
 
     }
+
+  }
+
+
+  /*
+  * Add Translations
+  * PD: New Alternative method due to problems with astronomic translatable
+  **/
+  public function addTranslation($paymentMethod, $locale, $title, $description)
+  {
+
+    \DB::table('icommerce__payment_method_translations')->insert([
+      'title' => $title,
+      'description' => trans($description, [], $locale),
+      'payment_method_id' => $paymentMethod->id,
+      'locale' => $locale
+    ]);
+
+  }
 
 }
